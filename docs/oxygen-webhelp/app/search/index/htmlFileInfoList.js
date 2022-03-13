5e81b35cfdc77fb2a55db2461ea196d7883925bb
AnnaWrob <92725367+AnnaBureta@users.noreply.github.com>
2022-03-13 20:16:36 +0100

    commit

--- docs/oxygen-webhelp/app/search/index/htmlFileInfoList.js
+++ docs/oxygen-webhelp/app/search/index/htmlFileInfoList.js
@@ -1,3 +1,3 @@
 define(function () {
-return ["cshelp.html@@@Context Sensitive Help@@@...","my_first_map.merged.html@@@my first map@@@...","topics/c_my_first_topic.html@@@my first topic@@@Table 1 . First...","topics/r_my_first_reference.html@@@my first reference@@@Table 1 . My first table This is my table description...","topics/t_doing_something.html@@@doing something@@@Required: Add a tag..."];
+return ["cshelp.html@@@Context Sensitive Help@@@...","my_first_map.merged.html@@@my first map@@@...","topics/c_my_first_topic.html@@@my first topic@@@Table 1 . This is my web page. http://www.sworn-translator.pl...","topics/r_my_first_reference.html@@@my first reference@@@Table 1 . My first table This is my table description...","topics/t_doing_something.html@@@doing something@@@Required: Add a tag..."];
 });
--- docs/oxygen-webhelp/app/search/index/index-1.js
+++ docs/oxygen-webhelp/app/search/index/index-1.js
@@ -1,4 +1,4 @@
 define(function () {
 // Auto generated index for searching.
-return {"add":"1*0*1v,4*10*-1$5","anna":"1*6*3$e$j$n$s$1c$1s","bureta":"1*6*4$f$k$o$t$1d$1t","context":"0*20*0","description":"1*0*1k,3*10*-1$c","doing":"1*11*l$17$1q,2*0*f,4*30*0$2","first":"1*49*1$6$9$c$h$q$v$13$1a$1g$1o$24,2*41*-1$1$4$7$b,3*41*-1$1$4$8$g"};
+return {"add":"1*0*27,4*10*-1$5","anna":"1*6*3$e$j$n$s$1k$24","bureta":"1*6*4$f$k$o$t$1l$25","context":"0*20*0","description":"1*0*1s,3*10*-1$c","doing":"1*11*l$1f$22,2*0*n,4*30*0$2","first":"1*48*1$6$9$c$h$q$1b$1i$1o$20$2c,2*31*1$4$j,3*41*-1$1$4$8$g,4*0*a","help":"0*20*2","http":"1*0*13,2*10*-1$b","http://www.sworn-translator.pl":"1*0*12,2*10*-1$a"};
 });

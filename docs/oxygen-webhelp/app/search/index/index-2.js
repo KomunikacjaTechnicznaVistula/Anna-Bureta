b3143b8085ff51adbf65ccbb3b7ef01e54b4b20f
AnnaWrob <92725367+AnnaBureta@users.noreply.github.com>
2022-03-13 11:57:08 +0100

    commit

---
+++ docs/oxygen-webhelp/app/search/index/index-2.js
@@ -0,0 +1,4 @@
+define(function () {
+// Auto generated index for searching.
+return {"help":"0*20*2","information":"1*15*16$1m$22,2*5*e,3*5*e","map":"1*22*2$7$a","my":"1*49*0$5$8$b$g$p$12$19$1f$1i$1n$23,2*31*0$3$a,3*51*-1$-1$0$3$7$a$f","reference":"1*16*i$11$14$1b,2*6*9$c,3*30*2$5","related":"1*20*10$15$1l$21,2*10*8$d,3*5*d","required":"1*5*1u,4*14*-1$4"};
+});

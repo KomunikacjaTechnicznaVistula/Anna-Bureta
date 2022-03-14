44ffffc3030e61ab460b0f6148819220525801ac
AnnaWrob <92725367+AnnaBureta@users.noreply.github.com>
2022-03-14 23:05:23 +0100

    fruits and veggies

--- docs/oxygen-webhelp/app/search/index/link-to-parent.js
+++ docs/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -1,4 +1,4 @@
 /*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
 define(function () {
-return {6:13,9:14,13:-1,8:13,3:13,7:14,4:14,14:-1,10:-1};
+return {3:-1,16:3,6:-1,7:-1,15:6,4:-1,10:-1,19:4,9:-1,17:9,14:8,8:-1,18:7};
 });
